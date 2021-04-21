4402dbc66ef5606bafff4834217cb4178e686b55
Chris Mander <mandercs3@gmail.com>
2021-04-21 13:55:07 +0100

    Moved random generation of tetros into free function. Tetrominoes vector now part of playfield class

--- main.cpp
+++ main.cpp
@@ -37,6 +37,22 @@ namespace
 	const TetroTemplate& t_template;
     };
 
+    Tetromino get_new_tetro(const int type, Vec&& pos)
+    {
+	auto which = [] (const char* val) { std::cout << "Added a " << val; };
+	switch(type)
+	{
+	case 0: which("i_tetro"); return Tetromino(std::move(pos), i_tetro);
+	case 1: which("j_tetro"); return Tetromino(std::move(pos), j_tetro);
+    	case 2: which("l_tetro"); return Tetromino(std::move(pos), l_tetro);
+    	case 3: which("o_tetro"); return Tetromino(std::move(pos), o_tetro);
+      	case 4: which("s_tetro"); return Tetromino(std::move(pos), s_tetro);
+       	case 5: which("t_tetro"); return Tetromino(std::move(pos), t_tetro);
+       	case 6: which("z_tetro"); return Tetromino(std::move(pos), z_tetro);
+	default:                  return Tetromino(std::move(pos), z_tetro);
+	}
+    }
+
     // The play field is ten blocks wide and fourty blocks high.
     // Only twenty of the vertical blocks are visible to the user.
     class PlayField
@@ -62,18 +78,22 @@ namespace
 	{
 	    return playfield[y * 10 + x];
 	}
-	
+
+	void add_tetro(Tetromino&& tetromino) noexcept
+	{
+	    tetrominoes.push_back(std::move(tetromino));
+	}	
+
+    private:
+	std::array<char, 40 * 10> playfield;
+	std::vector<Tetromino> tetrominoes;
+
 	void set(int x, int y, char val) noexcept
 	{
 	    playfield[y * 10 + x] = val;
-	}
-
-    private:
-	std::array<char, 40 * 10> playfield; 
+	}	
     };
-
-    std::vector<Tetromino> tetrominoes;    
-} 
+}
 
 int main()
 {
@@ -85,20 +105,8 @@ int main()
     for(;;)
     {
 	std::cin.ignore();
-	
-	// generate a random number between 0-6 inclusive to determine which tetromino to genrate
-	auto r = std::rand() % 7;
 
-	auto which = [] (const char* val) { std::cout << "Added a " << val; };
-	switch(r)
-	{
-	case 0: which("i_tetro"); tetrominoes.emplace_back(Vec(10, 10), i_tetro); break;
-	case 1: which("j_tetro"); tetrominoes.emplace_back(Vec(10, 10), i_tetro); break;
-    	case 2: which("l_tetro"); tetrominoes.emplace_back(Vec(10, 10), i_tetro); break;
-    	case 3: which("o_tetro"); tetrominoes.emplace_back(Vec(10, 10), i_tetro); break;
-      	case 4: which("s_tetro"); tetrominoes.emplace_back(Vec(10, 10), i_tetro); break;
-       	case 5: which("t_tetro"); tetrominoes.emplace_back(Vec(10, 10), i_tetro); break;
-       	case 6: which("z_tetro"); tetrominoes.emplace_back(Vec(10, 10), i_tetro); break;
-	}
+	// Generate new tetromino
+	auto current_tetro = get_new_tetro(std::rand() % 7, Vec(10, 10));
     }
 }
